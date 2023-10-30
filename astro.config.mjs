bacda1803ee0d751f18e1899c3f93f5c8f1ea1ff
William Hibberd <will@williamhibberd.co.uk>
2023-10-30 13:58:57 +0000

    add fonts, update copy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://venerable-licorice-d7b001.netlify.app/",
-  integrations: [tailwind(), sitemap()]
+	site: "https://venerable-licorice-d7b001.netlify.app/",
+	integrations: [tailwind({ applyBaseStyles: false }), sitemap()],
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 export default {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
+		fontFamily: {
+			sans: ['"america"', ...defaultTheme.fontFamily.serif],
+			cond: ['"america-cond"', ...defaultTheme.fontFamily.serif],
+			exp: ['"america-exp"', ...defaultTheme.fontFamily.serif],
+		},
 		extend: {},
 	},
 	plugins: [require("@tailwindcss/typography")],
