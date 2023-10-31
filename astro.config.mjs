6bef65684a2e35058df5eeec5933548356b89a5b
William Hibberd <will@williamhibberd.co.uk>
2023-10-31 12:55:19 +0000

    update site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://atlantica.art/",
+	site: "https://atlantica.art",
 	integrations: [tailwind({ applyBaseStyles: false }), sitemap()],
 });
